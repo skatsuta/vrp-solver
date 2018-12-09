2a57128ae458fc7678a93ac17340a4a0519c18be
Soshi Katsuta <soshi.katsuta@gmail.com>
2018-12-10 01:31:46 +0900

    Separate data from main code to JSON file and take JSON file path as an argument

--- cvrptw.py
+++ cvrptw.py
@@ -4,70 +4,21 @@
 Solve the Capacitated Vehicle Routing Problem with Time Windows (CVRPTW).
 """
 
+import argparse
+import json
 import pygraphviz as pgv
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
-def create_data_model() -> dict:
+def load_data_model(path: str) -> dict:
     """
-    Store the data for the problem.
+    Load the data for the problem from path.
     """
-    # Weights on each edge
-    weights = [
-        [0, 6, 9, 8, 6, 3, 6, 2, 3, 2, 6, 6, 4, 4, 5, 9, 7],
-        [6, 0, 8, 3, 2, 6, 8, 4, 8, 8, 13, 7, 5, 8, 12, 10, 14],
-        [9, 8, 0, 11, 10, 6, 3, 9, 5, 8, 4, 15, 13, 13, 9, 18, 9],
-        [8, 3, 11, 0, 1, 7, 10, 6, 10, 10, 14, 6, 7, 9, 13, 6, 16],
-        [6, 2, 10, 1, 0, 6, 9, 4, 8, 9, 13, 4, 6, 8, 12, 8, 14],
-        [3, 6, 6, 7, 6, 0, 2, 3, 2, 2, 6, 9, 7, 7, 6, 12, 8],
-        [6, 8, 3, 10, 9, 2, 0, 6, 2, 5, 4, 12, 10, 10, 6, 15, 5],
-        [2, 4, 9, 6, 4, 3, 6, 0, 4, 4, 8, 5, 4, 3, 7, 8, 10],
-        [3, 8, 5, 10, 8, 2, 2, 4, 0, 3, 4, 9, 8, 7, 3, 13, 6],
-        [2, 8, 8, 10, 9, 2, 5, 4, 3, 0, 4, 6, 5, 4, 3, 9, 5],
-        [6, 13, 4, 14, 13, 6, 4, 8, 4, 4, 0, 10, 9, 8, 4, 13, 4],
-        [6, 7, 15, 6, 4, 9, 12, 5, 9, 6, 10, 0, 1, 3, 7, 3, 10],
-        [4, 5, 13, 7, 6, 7, 10, 4, 8, 5, 9, 1, 0, 2, 6, 4, 8],
-        [4, 8, 13, 9, 8, 7, 10, 3, 7, 4, 8, 3, 2, 0, 4, 5, 6],
-        [5, 12, 9, 13, 12, 6, 6, 7, 3, 3, 4, 7, 6, 4, 0, 9, 2],
-        [9, 10, 18, 6, 8, 12, 15, 8, 13, 9, 13, 3, 4, 5, 9, 0, 9],
-        [7, 14, 9, 16, 14, 8, 5, 10, 6, 5, 4, 10, 8, 6, 2, 9, 0],
-    ]
+    with open(path) as file:
+        data = json.load(file)
 
-    # The number of people to ride
-    demands = [
-        0, # depot
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-        1, 1,
-    ]
+    data['num_locations'] = len(data['time_windows'])
+    data['num_vehicles'] = len(data['vehicle_capacities'])
 
-    capacities = [15, 15, 15]
-
-    time_windows = [
-        (0, 0),
-        (75, 95), (75, 95), # 1, 2
-        (60, 80), (45, 65), # 3, 4
-        (0, 20), (55, 75),  # 5, 6
-        (0, 20), (10, 30),  # 7, 8
-        (0, 20), (75, 95),  # 9, 10
-        (85, 105), (5, 25), # 11, 12
-        (15, 35), (10, 30), # 13, 14
-        (45, 65), (30, 50), # 15, 16
-    ]
-
-    data = {'weights': weights,
-            'num_locations': len(demands),
-            'num_vehicles': len(capacities),
-            'depot': 0,
-            'demands': demands,
-            'vehicle_capacities': capacities,
-            'time_windows': time_windows,
-            # Time in minutes to spend at each location for service (e.g. pickup)
-            'service_time': 5}
     return data
 
 def create_weight_callback(data):
@@ -110,11 +61,11 @@ def create_time_callback(data):
     """
     Create a callback to get total times between locations.
     """
-    def service_time(_):
+    def service_time(node):
         """
         Get the service time to the specified location.
         """
-        return data['service_time']
+        return data['service_times'][node]
 
     def travel_time(from_node, to_node):
         """
@@ -249,8 +200,13 @@ def main(enable_guided_local_search: bool = False, export_graph: bool = False):
     """
     Entry point of the program.
     """
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    args = parser.parse_args()
+
     # Instantiate the data problem
-    data = create_data_model()
+    data = load_data_model(args.path)
 
     # Create Routing Model
     routing = pywrapcp.RoutingModel(
